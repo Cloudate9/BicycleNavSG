f6d107f5e7ba07643f8a038ecfd7165ceffee1ec
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2025-06-02 22:22:15 +0800

    Redirect requests to onemap api

--- Server/cloudflare-workers.js
+++ Server/cloudflare-workers.js
@@ -1,7 +1,10 @@
 export default {
-  async fetch(request, env) {
-    async function getAuth(username, pw) {
-      const authRes = await fetch("https://www.onemap.gov.sg/api/auth/post/getToken", {
+  async fetch(request, env, ctx) {
+    const cache = caches.default;
+    const authDest = "https://www.onemap.gov.sg/api/auth/post/getToken";
+
+    async function fetchAuth(username, pw) {
+      let authRes = await fetch(authDest, {
         method: "POST",
         headers: {
         "Content-Type": "application/json",
@@ -12,10 +15,58 @@ export default {
         }),
       });
       if (authRes.ok) {
-        const jsonRes = await authRes.json();
-        return new Response(jsonRes.access_token);
+        // Need to make authRes mutable to change its Cache Control
+        authRes = new Response(authRes.body, authRes);
+        const jsonRes = await authRes.clone().json();
+
+        const timeNowSeconds = Math.floor(Date.now() / 1000);
+        const maxAge = (jsonRes.expiry_timestamp - timeNowSeconds) > 60 * 60 * 24 ? 60 * 60 * 24 : jsonRes.expiry_timestamp - timeNowSeconds;
+        authRes.headers.append("Cache-Control", `s-maxage=${maxAge}`);
+
+        ctx.waitUntil(cache.put(authDest, authRes.clone()));
+        return authRes; 
+      } else {
+        return null;
       }
     }
-    return await getAuth(env.ONEMAP_USERNAME, env.ONEMAP_PASSWORD);
+
+    // Validate url of final endpoint
+    const initUrl = new URL(request.url);
+    const apiDest = `https://www.onemap.gov.sg${initUrl.pathname}${initUrl.search}`;
+    // We don't want people to use our site to effectively surf the whole of onemap
+    if (!apiDest.startsWith("https://www.onemap.gov.sg/api/")) {
+      return new Response(null, { status: 404 });
+    }
+
+
+    // Now that we know that the endpoint could be legit, attempt to obtain an auth token
+    let auth = await cache.match(authDest); 
+
+    if (auth === undefined) {
+      auth = await fetchAuth(env.ONEMAP_USERNAME, env.ONEMAP_PASSWORD);
+    }
+    // Try to get auth key again
+    if (auth === null) {
+      auth = await fetchAuth(env.ONEMAP_USERNAME, env.ONEMAP_PASSWORD);
+    }
+    // If we are still unable to get an auth token, just give up
+    if (auth === null) {
+      return Response(null, { status: 401 });
+    }
+    const authJson = await auth.json();
+    const authToken = authJson.access_token;
+
+
+    // Actually hit the final endpoint
+    const apiRes = await fetch(apiDest, {
+      method: request.method,
+      headers: { ...request.headers, "Authorization": authToken, "Content-Type": "application/json" },
+    });
+    
+    if (apiRes.status === 404) {
+      return new Response(null, { status: 404 });
+    }
+
+    return apiRes;
   }
 };
